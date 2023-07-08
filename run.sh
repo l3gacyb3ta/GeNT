fe069c6372ce2f8e78efb0b5d11f96835ecfb8ff
veloya <archaic.archea@gmail.com>
2023-07-07 20:42:33 -0400

    updated a dir name updated gitignore to no longer ignore iso's Added limine config

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
+rm -rf ./.root
+
 if ! test -f "EDK2.fd"; then
     curl https://retrage.github.io/edk2-nightly/bin/RELEASERISCV64_VIRT.fd -o EDK2.fd
 fi
@@ -6,11 +8,11 @@ truncate EDK2.fd --size 33554432
 #TODO: Add OS bin
 git clone https://github.com/limine-bootloader/limine.git --depth 1 --branch=v5.x-branch-binary 
 make -C limine
-mkdir -p iso_root
-cp -v limine.cfg limine/limine-bios.sys \
-      limine/limine-bios-cd.bin limine/limine-uefi-cd.bin iso_root/
-mkdir -p iso_root/EFI/BOOT
-cp -v limine/BOOT*.EFI iso_root/EFI/BOOT/
+mkdir -p .root
+cp -v zig-out/bin/GeNT-kern config/limine.cfg limine/limine-bios.sys \
+      limine/limine-bios-cd.bin limine/limine-uefi-cd.bin .root/
+mkdir -p .root/EFI/BOOT
+cp -v limine/BOOTRISCV64.EFI .root/EFI/BOOT/
 
 qemu-system-riscv64 \
     -machine virt \
@@ -19,7 +21,7 @@ qemu-system-riscv64 \
     -m 512M \
     -drive if=pflash,format=raw,unit=1,file=EDK2.fd \
     -device nvme,serial=deadbeff,drive=disk1 \
-    -drive id=disk1,format=raw,if=none,file=fat:rw:./iso_root\
+    -drive id=disk1,format=raw,if=none,file=fat:rw:./.root\
     -global virtio-mmio.force-legacy=false \
     -device ramfb \
     -serial mon:stdio
