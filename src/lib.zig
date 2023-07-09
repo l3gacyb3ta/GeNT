c3abc17deac40374622a6d6d19d05cf9780f77a0
veloya <archaic.archea@gmail.com>
2023-07-09 17:24:46 -0400

    Added copyright stub to lib.zig

--- src/lib.zig
+++ src/lib.zig
@@ -1,3 +1,6 @@
+// © 2023 Lilly & GeNT Developers archaic.archea@gmail.com
+// License: [CNPL](https://git.pixie.town/thufie/npl-builder/raw/branch/main/cnpl.md)
+
 const std = @import("std");
 const stdio = @import("io/writer/stdio.zig");
 const stderr = @import("io/writer/stderr.zig");
